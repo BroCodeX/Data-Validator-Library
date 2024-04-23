cde7fbbedc3bac0c3ff2c81593ba6e02b6e9cc39
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-04-23 14:04:19 +0300

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,51 @@
+import com.adarshr.gradle.testlogger.theme.ThemeType
+
+plugins {
+    id("java")
+    `java-library`
+    application
+    id("checkstyle")
+    id("com.adarshr.test-logger") version "4.0.0"
+    id("io.freefair.lombok") version "8.4"
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("info.picocli:picocli:4.7.5")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.0")
+    implementation("com.puppycrawl.tools:checkstyle:10.15.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.16.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+testlogger {
+    showFullStackTraces = true
+    theme = ThemeType.MOCHA
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required = true
+    }
+}
