25cf6ea3e05038a1f36cb551067cf89fc804055b
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-05-02 08:36:54 +0300

    added final to test classes

--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class MapSchemaTest {
+public final class MapSchemaTest {
     private MapSchema mapSchema;
     private Validator validator;
 
