2bbf1249e768346409b7e43eed8173755816a036
Sergey Sukhanov <ss.sukhanoff@gmail.com>
2024-05-21 11:22:44 +0300

    refactoring 3.2 map validation refactoring

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -15,25 +15,33 @@ public final class MapSchema extends BaseSchema<Map<?, ?>> {
         return this;
     }
 
-    public void shape(Map<?, ?> schemas) {
+    public <S, T> MapSchema shape(Map<S, BaseSchema<T>> schemas) {
         addValidation("shape", value -> shapeHandler(value, schemas));
+        return this;
     }
 
     public boolean shapeHandler(Map<?, ?> value, Map<?, ?> schemas) {
         return value.entrySet().stream()
                 .allMatch(entry -> {
                     boolean result = false;
-                    if (entry.getKey() instanceof String) {
-                        String key = entry.getKey().toString();
-                        String val = entry.getValue() == null ? null : entry.getValue().toString();
-                        StringSchema check = (StringSchema) schemas.get(key);
-                        result = check.isValid(val);
-                    } else if (entry.getKey() instanceof Integer) {
-                        Integer key = (Integer) entry.getKey();
-                        Integer val = entry.getValue() == null ? null : (Integer) entry.getValue();
-                        NumberSchema check = (NumberSchema) schemas.get(key);
-                        result = check.isValid(val);
-                    }
+                    Object key = entry.getKey();
+                    Object val = entry.getValue() == null ? null : entry.getValue();
+                    BaseSchema<? extends BaseSchema> check = (BaseSchema<? extends BaseSchema>) schemas.get(key);
+                    result = check.isValid(val);
+                    return result;
+
+
+//                    if (entry.getKey() instanceof String) {
+//                        String key = entry.getKey().toString();
+//                        String val = entry.getValue() == null ? null : entry.getValue().toString();
+//                        StringSchema check = (StringSchema) schemas.get(key);
+//                        result = check.isValid(val);
+//                    } else if (entry.getKey() instanceof Integer) {
+//                        Integer key = (Integer) entry.getKey();
+//                        Integer val = entry.getValue() == null ? null : (Integer) entry.getValue();
+//                        NumberSchema check = (NumberSchema) schemas.get(key);
+//                        result = check.isValid(val);
+//                    }
                     return result;
                 });
     }
